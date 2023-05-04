424b0729f995f931b42eced8c7a379aae60759e4
Ada Qi <97003885+AdaQi-001@users.noreply.github.com>
2023-05-04 15:12:46 -0700

    explore_part_1

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,41 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+// window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+// function init() {
+//   // TODO
+// }
+
+const synth = window.speechSynthesis();
+
+const inputTxt = document.querySelector('#explore textarea');
+const voiceSelect = document.getElementById('voice-select');
+const button_talk = document.querySelector('#explore button');
+
+function populateVoiceList() {
+  if (typeof speechSynthesis === 'undefined') {
+    return;
+  }
+  const voices = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (
+  typeof speechSynthesis !== 'undefined' &&
+  speechSynthesis.onvoiceschanged !== undefined
+) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
